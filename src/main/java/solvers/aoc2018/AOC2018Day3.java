2d01252c86fcd437875d7f697ab80a346bd13a32
jontmy00 <jontmy001@gmail.com>
2021-07-23 15:13:46 +0800

    Add day 3 files

---
+++ src/main/java/solvers/aoc2018/AOC2018Day3.java
@@ -0,0 +1,27 @@
+package solvers.aoc2018;
+
+import solvers.AOCDay;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class AOC2018Day3 extends AOCDay<Integer> {
+    public AOC2018Day3() throws IOException, URISyntaxException {
+        super(3, 2018);
+    }
+
+    @Override
+    protected Integer solvePartOne(List<String> input) {
+        return null;
+    }
+
+    @Override
+    protected Integer solvePartTwo(List<String> input) {
+        return null;
+    }
+
+    private record Rectangle(int x, int y, int w, int h) {
+
+    }
+}
