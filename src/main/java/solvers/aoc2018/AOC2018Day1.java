1ec5199ff31aa35972980d07dde8a3d08488cdab
jontmy00 <jontmy001@gmail.com>
2021-07-23 11:50:52 +0800

    Solve day 1

--- src/main/java/solvers/aoc2018/AOC2018Day1.java
+++ src/main/java/solvers/aoc2018/AOC2018Day1.java
@@ -1,28 +1,37 @@
 package solvers.aoc2018;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
 import solvers.AOCDay;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.HashSet;
 import java.util.List;
 
-public class AOC2018Day1 extends AOCDay<String> {
-    private static final Logger LOGGER = (Logger) LogManager.getLogger(AOC2018Day1.class);
-
+public class AOC2018Day1 extends AOCDay<Integer> {
     public AOC2018Day1() throws IOException, URISyntaxException {
         super(1, 2018);
     }
 
     @Override
-    protected String solvePartOne(List<String> input) {
-        input.forEach(LOGGER::debug);
-        return "UNSOLVED";
+    protected Integer solvePartOne(List<String> input) {
+        return input.stream()
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
     @Override
-    protected String solvePartTwo(List<String> input) {
-        return "UNSOLVED";
+    protected Integer solvePartTwo(List<String> input) {
+        var changes = input.stream()
+                .map(Integer::valueOf)
+                .toList();
+        var frequencies = new HashSet<Integer>();
+        var current = 0;
+        frequencies.add(current);
+        for (int i = 0; ; i++) {
+            current += changes.get(i % changes.size());
+            if (frequencies.contains(current)) break;
+            else frequencies.add(current);
+        }
+        return current;
     }
 }
