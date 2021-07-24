21a619a93378b7d6e92a6d9a68c1f87a06216d9f
jontmy00 <jontmy001@gmail.com>
2021-07-24 11:19:14 +0800

    Solve day 5 part 2

--- src/main/java/solvers/aoc2018/AOC2018Day5.java
+++ src/main/java/solvers/aoc2018/AOC2018Day5.java
@@ -4,6 +4,7 @@ import solvers.AOCDay;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Comparator;
 import java.util.List;
 
 public class AOC2018Day5 extends AOCDay<Integer> {
@@ -14,18 +15,22 @@ public class AOC2018Day5 extends AOCDay<Integer> {
     @Override
     protected Integer solvePartOne(List<String> input) {
         assert !input.isEmpty() : "Missing input.";
-        var current = Polymer.of(input.get(0));
-        var reacted = current.react();
-        while (reacted.length() < current.length()) {
-            current = reacted;
-            reacted = reacted.react();
-        }
-        return reacted.length();
+        var initial = Polymer.of(input.get(0));
+        return initial.react().length();
     }
 
     @Override
     protected Integer solvePartTwo(List<String> input) {
-        return 0;
+        assert !input.isEmpty() : "Missing input.";
+        var initial = Polymer.of(input.get(0));
+        var units = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return units.chars()
+                .mapToObj(c -> (char) c)
+                .map(initial::without)
+                .map(Polymer::react)
+                .map(Polymer::length)
+                .min(Comparator.naturalOrder())
+                .orElseThrow(() -> new AssertionError("Missing polymer reaction result."));
     }
 
     private record Polymer(String chain) {
@@ -33,36 +38,50 @@ public class AOC2018Day5 extends AOCDay<Integer> {
             return new Polymer(chain);
         }
 
-        private static boolean reacts(char first, char second) {
+        private static boolean unitsReact(char first, char second) {
             if (Character.toLowerCase(first) != Character.toLowerCase(second)) {
                 return false; // different unit (character)
             }
             return first != second; // true if both are of different cases, false if both are of the same case
         }
 
-        private Polymer react() {
+        private static Polymer reactOnce(Polymer reactant) {
             var sb = new StringBuilder();
-            for (int i = 0; i < chain.length() - 1; ) {
-                char first = chain.charAt(i);
-                char second = chain.charAt(i + 1);
-                if (reacts(first, second)) {
+            for (int i = 0; i < reactant.length() - 1; ) {
+                char first = reactant.chain().charAt(i);
+                char second = reactant.chain().charAt(i + 1);
+                if (unitsReact(first, second)) {
                     i += 2;
                 } else {
                     sb.append(first);
                     i++;
                 }
             }
-            sb.append(chain.charAt(chain.length() - 1));
+            sb.append(reactant.chain().charAt(reactant.length() - 1));
             return Polymer.of(sb.toString());
+        }
+
+        private Polymer without(char alphabet) {
+            var reduction = chain.chars()
+                    .mapToObj(c -> (char) c)
+                    .filter(unit -> Character.toLowerCase(unit) != Character.toLowerCase(alphabet))
+                    .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
+                    .toString();
+            return Polymer.of(reduction);
+        }
+
+        private Polymer react() {
+            var current = this;
+            var reacted = reactOnce(current);
+            while (reacted.length() < current.length()) {
+                current = reacted;
+                reacted = reactOnce(reacted);
+            }
+            return reacted;
         }
 
         private int length() {
             return chain.length();
-        }
-
-        @Override
-        public String toString() {
-            return chain;
         }
     }
 }
