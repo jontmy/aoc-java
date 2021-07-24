fa3292b922b6ac2b1d052e40106fc9bc6b9c04e6
jontmy00 <jontmy001@gmail.com>
2021-07-24 10:59:31 +0800

    Solve day 5 part 1

--- src/main/java/solvers/aoc2018/AOC2018Day5.java
+++ src/main/java/solvers/aoc2018/AOC2018Day5.java
@@ -13,11 +13,56 @@ public class AOC2018Day5 extends AOCDay<Integer> {
 
     @Override
     protected Integer solvePartOne(List<String> input) {
-        return 0;
+        assert !input.isEmpty() : "Missing input.";
+        var current = Polymer.of(input.get(0));
+        var reacted = current.react();
+        while (reacted.length() < current.length()) {
+            current = reacted;
+            reacted = reacted.react();
+        }
+        return reacted.length();
     }
 
     @Override
     protected Integer solvePartTwo(List<String> input) {
         return 0;
+    }
+
+    private record Polymer(String chain) {
+        private static Polymer of(String chain) {
+            return new Polymer(chain);
+        }
+
+        private static boolean reacts(char first, char second) {
+            if (Character.toLowerCase(first) != Character.toLowerCase(second)) {
+                return false; // different unit (character)
+            }
+            return first != second; // true if both are of different cases, false if both are of the same case
+        }
+
+        private Polymer react() {
+            var sb = new StringBuilder();
+            for (int i = 0; i < chain.length() - 1; ) {
+                char first = chain.charAt(i);
+                char second = chain.charAt(i + 1);
+                if (reacts(first, second)) {
+                    i += 2;
+                } else {
+                    sb.append(first);
+                    i++;
+                }
+            }
+            sb.append(chain.charAt(chain.length() - 1));
+            return Polymer.of(sb.toString());
+        }
+
+        private int length() {
+            return chain.length();
+        }
+
+        @Override
+        public String toString() {
+            return chain;
+        }
     }
 }
