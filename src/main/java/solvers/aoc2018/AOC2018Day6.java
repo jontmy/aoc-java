308e3ab72bc75290ce7a00bb694d0567e5505a53
jontmy00 <jontmy001@gmail.com>
2021-07-24 14:30:08 +0800

    Solve day 6 part 1

--- src/main/java/solvers/aoc2018/AOC2018Day6.java
+++ src/main/java/solvers/aoc2018/AOC2018Day6.java
@@ -1,23 +1,231 @@
 package solvers.aoc2018;
 
 import solvers.AOCDay;
+import utils.Pair;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static utils.RegexUtils.*;
 
 public class AOC2018Day6 extends AOCDay<Integer> {
     public AOC2018Day6() throws IOException, URISyntaxException {
         super(6, 2018);
     }
 
+    private List<Destination> parse(List<String> input) {
+        var regex = join(group(min(ANY_DIGIT, 1)),
+                ", ", group(min(ANY_DIGIT, 1)));
+        var pattern = Pattern.compile(regex);
+        var unadjusted = input.stream()
+                .map(pattern::matcher)
+                .flatMap(matcher -> matcher.results()
+                        .findFirst()
+                        .stream())
+                .map(match -> Destination.ofCoordinates(
+                        Integer.parseInt(match.group(1)),
+                        Integer.parseInt(match.group(2))))
+                .toList();
+        var minX = unadjusted.stream()
+                .map(Destination::x)
+                .min(Comparator.naturalOrder())
+                .orElseThrow();
+        var minY = unadjusted.stream()
+                .map(Destination::y)
+                .min(Comparator.naturalOrder())
+                .orElseThrow();
+        var minXY = Destination.ofCoordinates(minX - 1, minY - 1);
+        return IntStream.range(0, unadjusted.size())
+                .mapToObj(id -> unadjusted.get(id).relative(minXY, id + 1))
+                .toList();
+    }
+
     @Override
     protected Integer solvePartOne(List<String> input) {
-        return 0;
+        var destinations = parse(input);
+        var map = new CoordinateMap(destinations);
+        return map.areasByID().get(map.largestAreaID()).intValue();
     }
 
     @Override
     protected Integer solvePartTwo(List<String> input) {
         return 0;
+    }
+
+    private static class CoordinateMap {
+        private final List<Destination> destinations;
+        private final int length, height;
+        private final int[][] map;
+
+        private CoordinateMap(List<Destination> destinations) {
+            this.destinations = List.copyOf(destinations);
+            this.length = destinations.stream()
+                    .map(Destination::x)
+                    .max(Comparator.naturalOrder())
+                    .orElseThrow() + 2;
+            this.height = destinations.stream()
+                    .map(Destination::y)
+                    .max(Comparator.naturalOrder())
+                    .orElseThrow() + 2;
+            this.map = new int[height][length];
+            plot();
+        }
+
+        // Returns true if the coordinate on the map lies in a finite area, or false if it lies in an infinite area.
+        // An area is finite if it is interrupted by another area (ID) in all 4 of the cardinal directions.
+        private static boolean finite(int x, int y, int[][] map) {
+            var id = map[y][x];
+            var height = map.length;
+            var length = map[0].length;
+            var west = Arrays.stream(map[y], 0, x)
+                    .anyMatch(areaID -> areaID != id);
+            var east = Arrays.stream(map[y], x + 1, length)
+                    .anyMatch(areaID -> areaID != id);
+            var north = IntStream.range(0, y)
+                    .map(i -> map[i][x])
+                    .anyMatch(areaID -> areaID != id);
+            var south = IntStream.range(y + 1, height)
+                    .map(i -> map[i][x])
+                    .anyMatch(areaID -> areaID != id);
+            return north && south && east && west;
+        }
+
+        // Returns the closest destination, or an empty optional if closest distance is tied between 2 destinations.
+        // Distance is calculated by Destination::manhattan.
+        private Optional<Destination> closestDestination(int x, int y) {
+            var manhattans = destinations.stream()
+                    .map(dest -> dest.manhattan(x, y))
+                    .sorted(Comparator.naturalOrder())
+                    .limit(2)
+                    .toList();
+            if (manhattans.get(0).equals(manhattans.get(1)))
+                return Optional.empty(); // closest distance tied between 2 destinations
+            var destination = destinations.stream()
+                    .filter(dest -> dest.manhattan(x, y) == manhattans.get(0))
+                    .toList();
+            assert destination.size() == 1;
+            return Optional.of(destination.get(0));
+        }
+
+        // Plots the coordinate map of the IDs of the closest destination from each coordinate, or -2 if tied,
+        // or -3 if the coordinate lies in an infinite area.
+        private void plot() {
+            var copy = new int[height][length];
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < length; j++) {
+                    var closestDestination = closestDestination(j, i);
+                    map[i][j] = closestDestination.map(Destination::id).orElse(-2);
+                    copy[i][j] = map[i][j];
+                }
+            }
+            // Check for coordinates in infinite areas.
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < length; j++) {
+                    if (map[i][j] < 0) continue;
+                    if (!finite(j, i, copy)) map[i][j] = -3;
+                }
+            }
+        }
+
+        private Map<Integer, Long> areasByID() {
+            return Arrays.stream(map)
+                    .flatMapToInt(Arrays::stream)
+                    .boxed()
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        }
+
+        private Integer largestAreaID() {
+            return areasByID().entrySet().stream()
+                    .filter(entry -> entry.getKey() > 0)
+                    .max(Map.Entry.comparingByValue())
+                    .map(Map.Entry::getKey)
+                    .orElseThrow();
+        }
+
+        @Override
+        public String toString() {
+            var dests = new int[height][length];
+            for (Destination destination : destinations) {
+                LOGGER.warn(destination);
+                dests[destination.y()][destination.x()] = destination.id;
+            }
+            var sb = new StringBuilder("\n");
+            for (int[] row : dests) {
+                for (int id : row) {
+                    if (id < 1) sb.append(".");
+                    else sb.append("%s".formatted(id));
+                }
+                sb.append("\n");
+            }
+            sb.append("\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < length; j++) {
+                    var closest = closestDestination(j, i);
+                    if (closest.isEmpty()) sb.append(".");
+                    else sb.append("%1d".formatted(closest.get().manhattan(j, i)));
+                }
+                sb.append("\n");
+            }
+            sb.append("\n");
+            for (int[] row : map) {
+                for (int id : row) {
+                    if (id == -2) sb.append(".");
+                    else if (id < 1) sb.append("*");
+                    else sb.append("%s".formatted(id));
+                }
+                sb.append("\n");
+            }
+            sb.append("\n");
+            for (int[] row : map) {
+                for (int id : row) {
+                    if (id == -2) sb.append(".");
+                    else if (id < 1) sb.append(" ");
+                    else sb.append("%s".formatted(id));
+                }
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
+    }
+
+    private record Destination(Pair<Integer, Integer> coordinates, int id) {
+        private Destination(Pair<Integer, Integer> coordinates, int id) {
+            this.coordinates = Pair.from(coordinates);
+            this.id = id;
+        }
+
+        private static Destination ofCoordinates(int x, int y) {
+            return new Destination(Pair.of(x, y), -1);
+        }
+
+        private static Destination ofCoordinatesAndID(int x, int y, int id) {
+            return new Destination(Pair.of(x, y), id);
+        }
+
+        private Destination relative(Destination that, int newID) {
+            return Destination.ofCoordinatesAndID(this.x() - that.x(), this.y() - that.y(), newID);
+        }
+
+        private int manhattan(int x, int y) {
+            return Math.abs(this.x() - x) + Math.abs(this.y() - y);
+        }
+
+        private int x() {
+            return coordinates.left();
+        }
+
+        private int y() {
+            return coordinates.right();
+        }
+
+        @Override
+        public String toString() {
+            return id() + " -> " + coordinates;
+        }
     }
 }
