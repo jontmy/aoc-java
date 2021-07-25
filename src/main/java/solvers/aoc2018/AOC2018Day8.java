8536a492283713361c380be419473298c213b7e0
jontmy00 <jontmy001@gmail.com>
2021-07-25 11:24:09 +0800

    Refactor shared code out of part 1

--- src/main/java/solvers/aoc2018/AOC2018Day8.java
+++ src/main/java/solvers/aoc2018/AOC2018Day8.java
@@ -1,7 +1,6 @@
 package solvers.aoc2018;
 
 import solvers.AOCDay;
-import utils.Pair;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -10,43 +9,44 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AOC2018Day8 extends AOCDay<Integer> {
-    private final List<Integer> license;
+    private final List<Integer> data;
+    private final List<Node> license;
 
     public AOC2018Day8() throws IOException, URISyntaxException {
         super(8, 2018);
         assert input.size() == 1 : "Malformed input.";
-        this.license = Arrays.stream(input.get(0).split(" "))
+        this.data = Arrays.stream(input.get(0).split(" "))
                 .map(Integer::valueOf)
                 .toList();
+        this.license = new ArrayList<>();
+        findEndPoint(0);
     }
 
-    private int findEndPoint(int startPoint, List<Node> nodes) {
-        var nChildren = license.get(startPoint);
+    private int findEndPoint(int startPoint) {
+        var nChildren = data.get(startPoint);
         assert nChildren >= 0 : "Zero or more child nodes must be specified.";
-        var nMetadata = license.get(startPoint + 1);
+        var nMetadata = data.get(startPoint + 1);
         assert nMetadata >= 1 : "One or more metadata entries must be specified.";
         var endPoint = startPoint + 2;
 
         // Base case: 0 child nodes.
         if (nChildren == 0) {
-            nodes.add(Node.of(startPoint, endPoint, endPoint, endPoint + nMetadata));
+            license.add(Node.of(startPoint, endPoint, endPoint, endPoint + nMetadata));
             return endPoint + nMetadata;
         }
 
         // Recursive case: 1 or more child nodes.
         for (int i = 0; i < nChildren; i++) {
-            endPoint = findEndPoint(endPoint, nodes);
+            endPoint = findEndPoint(endPoint);
         }
-        nodes.add(Node.of(startPoint, startPoint + 2, endPoint, endPoint + nMetadata));
+        license.add(Node.of(startPoint, startPoint + 2, endPoint, endPoint + nMetadata));
         return endPoint + nMetadata;
     }
 
     @Override
     protected Integer solvePartOne(List<String> input) {
-        var nodes = new ArrayList<Node>();
-        findEndPoint(0, nodes);
-        return nodes.stream()
-                .map(node -> license.subList(node.metadataStart(), node.endPoint()))
+        return license.stream()
+                .map(node -> data.subList(node.metadataStart(), node.endPoint()))
                 .flatMap(List::stream)
                 .reduce(Integer::sum)
                 .orElseThrow(() -> new AssertionError("Missing checksum."));
