91a8663b2901f380063154d7b3a5f4c5cae8bdf9
jontmy00 <jontmy001@gmail.com>
2021-07-25 09:49:50 +0800

    Add day 8 files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,8 @@ public class Main {
         new AOC2018Day4().solve();
         new AOC2018Day5().solve();
         new AOC2018Day6().solve();
-        */
         new AOC2018Day7().solve();
+        */
+        new AOC2018Day8().solve();
     }
 }
---
+++ src/main/java/solvers/aoc2018/AOC2018Day8.java
@@ -0,0 +1,23 @@
+package solvers.aoc2018;
+
+import solvers.AOCDay;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class AOC2018Day8 extends AOCDay<Integer> {
+    public AOC2018Day8() throws IOException, URISyntaxException {
+        super(8, 2018);
+    }
+
+    @Override
+    protected Integer solvePartOne(List<String> input) {
+        return 0;
+    }
+
+    @Override
+    protected Integer solvePartTwo(List<String> input) {
+        return 0;
+    }
+}
