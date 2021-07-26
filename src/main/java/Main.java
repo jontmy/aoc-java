44741f017494b1d5f8ade91b962ff73bc577fc4a
jontmy00 <jontmy001@gmail.com>
2021-07-26 09:31:59 +0800

    Add day 10 files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,8 @@ public class Main {
         new AOC2018Day6().solve();
         new AOC2018Day7().solve();
         new AOC2018Day8().solve();
-        */
         new AOC2018Day9().solve();
+        */
+        new AOC2018Day10().solve();
     }
 }
---
+++ src/main/java/solvers/aoc2018/AOC2018Day10.java
@@ -0,0 +1,23 @@
+package solvers.aoc2018;
+
+import solvers.AOCDay;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class AOC2018Day10 extends AOCDay<String> {
+    public AOC2018Day10() throws IOException, URISyntaxException {
+        super(10, 2018);
+    }
+
+    @Override
+    protected String solvePartOne(List<String> input) {
+        return "";
+    }
+
+    @Override
+    protected String solvePartTwo(List<String> input) {
+        return "";
+    }
+}
