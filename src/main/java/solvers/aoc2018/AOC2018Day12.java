35ac6b2c9eb3160fb88772e2a96b46dbaba6e8d6
jontmy00 <jontmy001@gmail.com>
2021-07-26 16:10:33 +0800

    Solve day 12 part 2

--- src/main/java/solvers/aoc2018/AOC2018Day12.java
+++ src/main/java/solvers/aoc2018/AOC2018Day12.java
@@ -12,25 +12,26 @@ import java.util.stream.IntStream;
 
 import static utils.RegexUtils.*;
 
-public class AOC2018Day12 extends AOCDay<Integer> {
+public class AOC2018Day12 extends AOCDay<Long> {
     private static final char ALIVE = '#', DEAD = '.';
-    private final TreeMap<Integer, Character> state;
+    private final Map<Integer, Character> state;
     private final Set<String> lives, dies;
+    private int lo, hi;
 
     public AOC2018Day12() throws IOException, URISyntaxException {
         super(12, 2018);
         assert input.size() > 1;
-        this.state = new TreeMap<>();
+        this.state = new HashMap<>();
         parseInitialState(state, input.get(0));
         this.lives = new HashSet<>();
         this.dies = new HashSet<>();
         parseRules(lives, dies, input.subList(1, input.size()));
-        // assert lives.size() + dies.size() == 32;
+        assert lives.size() + dies.size() == 32;
     }
 
     private static void parseInitialState(Map<Integer, Character> state, String input) {
         assert input.startsWith("initial state: ");
-        var raw = input.replaceAll("initial state: ", "").trim();
+        var raw = input.replaceAll("initial state: ", "").strip();
         for (int i = 0; i < raw.length(); i++) {
             char c = raw.charAt(i);
             assert c == ALIVE || c == DEAD;
@@ -38,7 +39,7 @@ public class AOC2018Day12 extends AOCDay<Integer> {
         }
     }
 
-    private static void parseRules(Set<String > lives, Set<String> dies, List<String> input) {
+    private static void parseRules(Set<String> lives, Set<String> dies, List<String> input) {
         var regex = join(group(repeat(set(or("#", ".")), 5, 5)),
                 " => ", group(or("#", ".")));
         var pattern = Pattern.compile(regex);
@@ -57,41 +58,43 @@ public class AOC2018Day12 extends AOCDay<Integer> {
         }
     }
 
-    private static char simulate(int id, TreeMap<Integer, Character> state,  Set<String> lives, Set<String> dies) {
+    private char simulate(int id, Map<Integer, Character> simulation) {
         var condition = IntStream.rangeClosed(id - 2, id + 2)
-                .mapToObj(i -> state.getOrDefault(i, DEAD))
+                .mapToObj(i -> simulation.getOrDefault(i, DEAD))
                 .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
                 .toString();
-        // assert lives.contains(condition) || dies.contains(condition);
+        assert lives.contains(condition) || dies.contains(condition);
         if (lives.contains(condition)) return ALIVE;
         else return DEAD;
     }
 
-    private static Map<Integer, Character> simulate(TreeMap<Integer, Character> state, Set<String> lives, Set<String> dies) {
-        int lo = state.firstKey() - 2, hi = state.lastKey() + 2;
-        var changes = new TreeMap<Integer, Character>();
-        for (int id = lo; id < hi; id++) {
-            var outcome = simulate(id, state, lives, dies);
-            if (state.getOrDefault(id, DEAD) != outcome) changes.put(id, outcome);
-        }
+    private Map<Integer, Character> simulate(Map<Integer, Character> simulation) {
+        var changes = Collections.synchronizedMap(new HashMap<Integer, Character>());
+        char l1 = simulate(lo - 1, simulation), l2 = simulate(lo - 2, simulation);
+        char h1 = simulate(hi + 1, simulation), h2 = simulate(hi + 2, simulation);
+        if (l1 == ALIVE) changes.put(--lo, l1);
+        if (l2 == ALIVE) changes.put(--lo, l2);
+        if (h1 == ALIVE) changes.put(++hi, h1);
+        if (h2 == ALIVE) changes.put(++hi, h2);
+        IntStream.range(lo, hi)
+                .parallel()
+                .mapToObj(id -> Pair.of(id, simulate(id, simulation)))
+                .forEach(pair -> {
+                    var id = pair.left();
+                    var outcome = pair.right();
+                    if (simulation.getOrDefault(id, DEAD) != outcome) changes.put(id, outcome);
+                });
         return changes;
     }
 
-    public static String stringify(Map<Integer, Character> state) {
-        return state.values().stream()
-                .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
-                .toString();
-    }
-
-    @Override
-    protected Integer solvePartOne(List<String> input) {
-        final int GENERATIONS = 20;
-        LOGGER.debug(stringify(state));
-        for (int i = 0; i < GENERATIONS; i++) {
-            state.putAll(simulate(state, lives, dies));
-            LOGGER.debug(stringify(state));
+    private Integer simulate(long generations) {
+        this.lo = 0;
+        this.hi = state.size() - 1;
+        var simulation = new HashMap<>(state);
+        for (long i = 0; i < generations; i++) {
+            simulation.putAll(simulate(simulation));
         }
-        return state.entrySet().stream()
+        return simulation.entrySet().stream()
                 .filter(kv -> kv.getValue() == ALIVE)
                 .map(Map.Entry::getKey)
                 .reduce(Integer::sum)
@@ -99,7 +102,22 @@ public class AOC2018Day12 extends AOCDay<Integer> {
     }
 
     @Override
-    protected Integer solvePartTwo(List<String> input) {
-        return 0;
+    protected Long solvePartOne(List<String> input) {
+        return simulate(20).longValue();
+    }
+
+    @Override
+    protected Long solvePartTwo(List<String> input) {
+        var generations = IntStream.range(0, 5)
+                .map(i -> i *= 100)
+                .map(this::simulate)
+                .boxed()
+                .toList();
+        var deltas = IntStream.range(1, 5)
+                .map(i -> generations.get(i) - generations.get(i - 1))
+                .boxed()
+                .toList();
+        assert deltas.subList(2, 4).stream().allMatch(delta -> deltas.get(1).equals(delta));
+        return simulate(500) + (50000000000L - 500) / 100 * deltas.get(1);
     }
 }
