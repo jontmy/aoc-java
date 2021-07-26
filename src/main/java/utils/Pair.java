a6360e515cd63ab7dffe2e56e5dd0ea8887ae578
jontmy00 <jontmy001@gmail.com>
2021-07-26 16:10:58 +0800

    Add Pair::map()

--- src/main/java/utils/Pair.java
+++ src/main/java/utils/Pair.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.function.Function;
+
 public record Pair<L, R>(L left, R right) {
     public static <L, R> Pair<L, R> of(L left, R right) {
         return new Pair<>(left, right);
@@ -7,6 +9,12 @@ public record Pair<L, R>(L left, R right) {
 
     public static <L, R> Pair<L, R> from(Pair<L, R> pair) {
         return new Pair<>(pair.left(), pair.right());
+    }
+
+    public <S, T> Pair<S, T> map(Function<L, S> leftMapper, Function<R, T> rightMapper) {
+        var left = leftMapper.apply(this.left());
+        var right = rightMapper.apply(this.right());
+        return Pair.of(left, right);
     }
 
     @Override
