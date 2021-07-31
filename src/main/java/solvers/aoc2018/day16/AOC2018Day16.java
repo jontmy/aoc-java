fb19e3b6aa04c3169e482af46f79ad846987329c
jontmy00 <jontmy001@gmail.com>
2021-07-31 13:24:39 +0800

    Solve day 16 part 2

--- src/main/java/solvers/aoc2018/day16/AOC2018Day16.java
+++ src/main/java/solvers/aoc2018/day16/AOC2018Day16.java
@@ -5,17 +5,23 @@ import solvers.AOCDay;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.util.function.Predicate.not;
 
 public class AOC2018Day16 extends AOCDay<Integer> {
     private final List<Simulator> simulators;
+    private final List<Instruction> instructions;
 
     public AOC2018Day16() throws IOException, URISyntaxException {
         super(16, 2018);
         this.simulators = new ArrayList<>();
+        this.instructions = new ArrayList<>();
         parse();
     }
 
@@ -26,7 +32,7 @@ public class AOC2018Day16 extends AOCDay<Integer> {
                 .orElseThrow();
 
         // Parse the input for the first part.
-        var first = IntStream.rangeClosed(0, split)
+        var first = IntStream.rangeClosed(0, split + 1)
                 .mapToObj(input::get)
                 .filter(not(String::isBlank))
                 .toList();
@@ -36,6 +42,13 @@ public class AOC2018Day16 extends AOCDay<Integer> {
             var after = Registers.parse(first.get(i++));
             simulators.add(new Simulator(before, instruction, after));
         }
+
+        // Parse the input for the second part.
+        IntStream.range(split + 1, input.size())
+                .mapToObj(input::get)
+                .filter(not(String::isBlank))
+                .map(Instruction::parse)
+                .forEach(instructions::add);
     }
 
     @Override
@@ -48,6 +61,41 @@ public class AOC2018Day16 extends AOCDay<Integer> {
 
     @Override
     protected Integer solvePartTwo(List<String> input) {
-        return 0;
+        // Map each opcode to its operation a.k.a. higher order instruction a.k.a. BiConsumer<Registers, Instruction>.
+        var unconfirmed = simulators.stream()
+                .map(Simulator::instruction)
+                .map(Instruction::opcode)
+                .distinct()
+                .collect(Collectors.toMap(op -> op, op -> new HashSet<>(Simulator.INSTRUCTIONS)));
+        var confirmed = new HashMap<Integer, BiConsumer<Registers, Instruction>>();
+
+        // Reduce each opcode mapping to just the operations that all simulations agree with by set intersections.
+        for (Simulator simulator : simulators) {
+            var opcode = simulator.instruction().opcode();
+            var operations = simulator.operations();
+            assert unconfirmed.containsKey(opcode);
+            unconfirmed.get(opcode).retainAll(operations);
+        }
+
+        // Work out the number of each opcode by elimination.
+        while (!unconfirmed.isEmpty()) {
+            for (Integer opcode : unconfirmed.keySet()) {
+                assert unconfirmed.containsKey(opcode);
+                assert unconfirmed.get(opcode).size() > 0;
+
+                // Confirm the opcode mapping if the number of its possible operations is 1.
+                if (unconfirmed.get(opcode).size() == 1) {
+                    var operation = unconfirmed.get(opcode).iterator().next();
+                    confirmed.put(opcode, operation);
+                }
+            }
+            confirmed.keySet().forEach(unconfirmed::remove);
+            unconfirmed.values().forEach(ops -> ops.removeAll(confirmed.values()));
+        }
+
+        // Run the program through the list of instructions.
+        var registers = Registers.blank();
+        instructions.forEach(ins -> confirmed.get(ins.opcode()).accept(registers, ins));
+        return registers.dereference(0);
     }
 }
--- src/main/java/solvers/aoc2018/day16/Simulator.java
+++ src/main/java/solvers/aoc2018/day16/Simulator.java
@@ -1,24 +1,20 @@
 package solvers.aoc2018.day16;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.BiConsumer;
 
 record Simulator(Registers before, Instruction instruction, Registers after) {
-    protected static final Logger LOGGER = (Logger) LogManager.getLogger(Simulator.class);
-    private static final List<BiConsumer<Registers, Instruction>> INSTRUCTIONS =
-            List.of(Instructions::addr, Instructions::addi,
+    protected static final Set<BiConsumer<Registers, Instruction>> INSTRUCTIONS =
+            Set.of(Instructions::addr, Instructions::addi,
                     Instructions::mulr, Instructions::muli, Instructions::banr, Instructions::bani,
                     Instructions::borr, Instructions::bori, Instructions::setr, Instructions::seti,
                     Instructions::gtir, Instructions::gtri, Instructions::gtrr,
                     Instructions::eqir, Instructions::eqri, Instructions::eqrr
             );
 
-    protected List<BiConsumer<Registers, Instruction>> operations() {
-        var matching = new ArrayList<BiConsumer<Registers, Instruction>>();
+    protected Set<BiConsumer<Registers, Instruction>> operations() {
+        var matching = new HashSet<BiConsumer<Registers, Instruction>>();
         for (BiConsumer<Registers, Instruction> operation : INSTRUCTIONS) {
             var simulable = Registers.from(before);
             operation.accept(simulable, instruction);
